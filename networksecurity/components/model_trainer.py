20b790275f4478ead0846e01caf95e77595998bf
akkiyolo <akshatshukla069@gmail.com>
2026-07-09 23:00:40 +0530

    prams updated

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -39,6 +39,33 @@ class ModelTrainer:
       "AdaBoost":AdaBoostClassifier(),
 
     }
+
+    params = {
+            "Decision Tree": {
+                'criterion': ['gini', 'entropy', 'log_loss'], 
+                # 'splitter':['best','random'],           
+                # 'max_features':['sqrt','log2'],         
+            },
+            "Random Forest": {
+                # 'criterion':['gini', 'entropy', 'log_loss'],
+                # 'max_features':['sqrt','log2',None],
+                'n_estimators': [8, 16, 32, 128, 256]  
+            },
+            "Gradient Boosting": {
+                # 'loss':['log_loss', 'exponential'],
+                'learning_rate': [.1, .01, .05, .001],
+                'subsample': [0.6, 0.7, 0.75, 0.85, 0.9],  
+                # 'criterion':['squared_error', 'friedman_mse'],
+                # 'max_features':['auto','sqrt','log2'],
+                'n_estimators': [8, 16, 32, 64, 128, 256]  
+            },
+            "Logistic Regression": {},  
+            "AdaBoost": {
+                'learning_rate': [.1, .01, .001],  
+                'n_estimators': [8, 16, 32, 64, 128, 256]  
+            }
+            
+    }
     
     
   def initiate_model_trainer(self)->ModelTrainerArtifact:
